8cbdfc9caa08f3c376a1889649b412f9742f43c8
zelincmput301 <zelin@ualberta.ca>
2017-02-25 18:59:02 -0700

    Android Studio Files Recreated

---
+++ Moodr/app/src/main/java/com/cmput301w17t08/moodr/Mood.java
@@ -0,0 +1,10 @@
+package com.cmput301w17t08.moodr;
+
+import java.util.Date;
+
+/**
+ * Created by ZL on 2/25/2017.
+ */
+public class Mood {
+
+}
---
+++ Moodr/app/src/main/java/com/cmput301w17t08/moodr/User.java
@@ -0,0 +1,7 @@
+package com.cmput301w17t08.moodr;
+
+/**
+ * Created by ZL on 2/25/2017.
+ */
+public class User {
+}
