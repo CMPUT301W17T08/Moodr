a69ef9b8bd4d2037c7663ee5d75441440f4ddf94
Canopus Tong <tongtongcanopus@gmail.com>
2017-03-03 16:42:23 -0700

    Added Lobster custom font

--- Moodr/app/src/main/java/com/cmput301w17t08/moodr/LogInActivity.java
+++ Moodr/app/src/main/java/com/cmput301w17t08/moodr/LogInActivity.java
@@ -3,6 +3,9 @@ package com.cmput301w17t08.moodr;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+// http://stackoverflow.com/questions/27588965/how-to-use-custom-font-in-android-studio
+// http://www.1001fonts.com/lobster-font.html
+
 public class LogInActivity extends AppCompatActivity {
 
     @Override
