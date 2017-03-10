b81f1e99c11d25fcd9a470fb7eb986bad55ace0c
MauriceGao <MauriceG@MauriceGaodeMacBook-Air.local>
2017-03-09 18:18:37 -0700

    MainActivity modified

--- Moodr/app/src/main/java/com/cmput301w17t08/moodr/LoginActivity.java
+++ Moodr/app/src/main/java/com/cmput301w17t08/moodr/LoginActivity.java
@@ -3,6 +3,8 @@ package com.cmput301w17t08.moodr;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -72,6 +74,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         mPasswordView = (EditText) findViewById(R.id.password);
         mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+
+
             @Override
             public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
                 if (id == R.id.login || id == EditorInfo.IME_NULL) {
