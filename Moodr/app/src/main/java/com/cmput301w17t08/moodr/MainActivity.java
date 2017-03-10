870c724f54c197eefa2e422f33bde6c608f33b67
MauriceGao <MauriceG@MauriceGaodeMacBook-Air.local>
2017-03-09 18:45:50 -0700

    Home icon

--- Moodr/app/src/main/java/com/cmput301w17t08/moodr/MainActivity.java
+++ Moodr/app/src/main/java/com/cmput301w17t08/moodr/MainActivity.java
@@ -45,7 +45,6 @@ public class MainActivity extends AppCompatActivity
 
 
 
-
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
