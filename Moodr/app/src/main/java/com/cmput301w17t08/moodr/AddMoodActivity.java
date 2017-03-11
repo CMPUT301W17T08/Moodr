935f7bed893589f6197a0882b21ef3ddafa6f876
Unknown <salvindr@ualberta.ca>
2017-03-10 21:18:11 -0700

    changes

--- Moodr/app/src/main/java/com/cmput301w17t08/moodr/AddMoodActivity.java
+++ Moodr/app/src/main/java/com/cmput301w17t08/moodr/AddMoodActivity.java
@@ -1,13 +1,13 @@
 package com.cmput301w17t08.moodr;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import java.util.ArrayList;
 
-public class AddMoodActivity {
-    private ArrayList<Mood> moodArrayList;
+public class AddMoodActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- Moodr/app/src/main/java/com/cmput301w17t08/moodr/EditMoodActivity.java
+++ Moodr/app/src/main/java/com/cmput301w17t08/moodr/EditMoodActivity.java
@@ -1,9 +1,10 @@
 package com.cmput301w17t08.moodr;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class EditMoodActivity {
+public class EditMoodActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
