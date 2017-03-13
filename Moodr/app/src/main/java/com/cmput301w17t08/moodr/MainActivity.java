0b8d5b87431c2f8c711b1ba2dcdb17679aa798a8
Mauricegao <yunzhi@ualberta.ca>
2017-03-13 12:48:38 -0600

    Temporary put login page to be called when clicking "log out"

--- Moodr/app/src/main/java/com/cmput301w17t08/moodr/MainActivity.java
+++ Moodr/app/src/main/java/com/cmput301w17t08/moodr/MainActivity.java
@@ -33,8 +33,8 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-        startActivity(intent);
+//         Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+//         startActivity(intent);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
