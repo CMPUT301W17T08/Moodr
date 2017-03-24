e45b252c53f4907c655ae826a0c14f541d9f5f83
Mauricegao <yunzhi@ualberta.ca>
2017-03-24 17:48:23 -0600

    Maps shown

--- Moodr/app/src/main/java/com/cmput301w17t08/moodr/MapsActivity.java
+++ Moodr/app/src/main/java/com/cmput301w17t08/moodr/MapsActivity.java
@@ -1,11 +1,7 @@
 package com.cmput301w17t08.moodr;
 
-import android.content.Intent;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
+import android.support.v4.app.FragmentActivity;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -21,77 +17,19 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_my_profile, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_home) {
-            Intent intent = new Intent(MapsActivity.this, MyProfileActivity.class);
-            startActivity(intent);
-            return true;
-        }
-
-        if (id == R.id.action_friends) {
-            Intent intent = new Intent(MapsActivity.this, FriendsActivity.class);
-            startActivity(intent);
-            return true;
-        }
-
-        if (id == R.id.action_nearme) {
-            Intent intent = new Intent(MapsActivity.this, MapsActivity.class);
-            startActivity(intent);
-            return true;
-        }
-
-        if (id == R.id.action_offline) {
-            Intent intent = new Intent(MapsActivity.this, OfflineMode.class);
-            startActivity(intent);
-            return true;
-        }
-
-        if (id == R.id.action_logout) {
-            Intent intent = new Intent(MapsActivity.this, LoginActivity.class);
-            startActivity(intent);
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng edmonton = new LatLng(53, -113);
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Edmonton"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(edmonton));
     }
 }
+
