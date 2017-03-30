03652d48be2ffaaa4b107b231abd982634dee5c0
Mauricegao <yunzhi@ualberta.ca>
2017-03-29 18:36:11 -0600

    Maps update

--- Moodr/app/src/main/java/com/cmput301w17t08/moodr/MapsActivity.java
+++ Moodr/app/src/main/java/com/cmput301w17t08/moodr/MapsActivity.java
@@ -1,5 +1,6 @@
 package com.cmput301w17t08.moodr;
 
+import android.*;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.support.annotation.NonNull;
@@ -7,6 +8,8 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
@@ -22,165 +25,102 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
-            GoogleApiClient.ConnectionCallbacks,
-            GoogleApiClient.OnConnectionFailedListener,
-            GoogleMap.OnMarkerDragListener,
-            GoogleMap.OnMapLongClickListener,
-            GoogleMap.OnMarkerClickListener,
-            View.OnClickListener {
+public class MapsActivity extends AppCompatActivity
+        implements
+        GoogleMap.OnMyLocationButtonClickListener,
+        OnMapReadyCallback,
+        ActivityCompat.OnRequestPermissionsResultCallback {
 
-        private static final String TAG = "MapsActivity";
-        private GoogleMap mMap;
-        private double longitude;
-        private double latitude;
-        private GoogleApiClient googleApiClient;
+    /**
+     * Request code for location permission request.
+     *
+     * @see #onRequestPermissionsResult(int, String[], int[])
+     */
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
 
+    /**
+     * Flag indicating whether a requested permission has been denied after returning in
+     * {@link #onRequestPermissionsResult(int, String[], int[])}.
+     */
+    private boolean mPermissionDenied = false;
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_maps);
+    private GoogleMap mMap;
 
-            // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                    .findFragmentById(R.id.map);
-            mapFragment.getMapAsync(this);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
 
-            //Initializing googleApiClient
-            googleApiClient = new GoogleApiClient.Builder(this)
-                    .addConnectionCallbacks(this)
-                    .addOnConnectionFailedListener(this)
-                    .addApi(LocationServices.API)
-                    .build();
-        }
-
-
-        @Override
-        public void onMapReady(GoogleMap googleMap) {
-            mMap = googleMap;
-            mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-            // googleMapOptions.mapType(googleMap.MAP_TYPE_HYBRID)
-            //    .compassEnabled(true);
-
-            // Add a marker in Sydney and move the camera
-            LatLng edmonton = new LatLng(53,-113);
-            mMap.addMarker(new MarkerOptions().position(edmonton).title("Marker in Edmonton"));
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(edmonton));
-            mMap.setOnMarkerDragListener(this);
-            mMap.setOnMapLongClickListener(this);
-        }
-
-
-
-
-
-        //Getting current location
-        private void getCurrentLocation() {
-            mMap.clear();
-            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return;
-            }
-            Location location = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
-            if (location != null) {
-                //Getting longitude and latitude
-                longitude = location.getLongitude();
-                latitude = location.getLatitude();
-
-                //moving the map to location
-                moveMap();
-            }
-        }
-
-        private void moveMap() {
-            /**
-             * Creating the latlng object to store lat, long coordinates
-             * adding marker to map
-             * move the camera with animation
-             */
-            LatLng latLng = new LatLng(latitude, longitude);
-            mMap.addMarker(new MarkerOptions()
-                    .position(latLng)
-                    .draggable(true)
-                    .title("Marker in Edmonton"));
-
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-            mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
-            mMap.getUiSettings().setZoomControlsEnabled(true);
-
-
-        }
-
-        @Override
-        public void onClick(View view) {
-            Log.v(TAG,"view click event");
-        }
-
-        @Override
-        public void onConnected(@Nullable Bundle bundle) {
-            getCurrentLocation();
-        }
-
-        @Override
-        public void onConnectionSuspended(int i) {
-
-        }
-
-        @Override
-        public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
-        }
-
-        @Override
-        public void onMapLongClick(LatLng latLng) {
-            // mMap.clear();
-            mMap.addMarker(new MarkerOptions().position(latLng).draggable(true));
-        }
-
-        @Override
-        public void onMarkerDragStart(Marker marker) {
-            Toast.makeText(MapsActivity.this, "onMarkerDragStart", Toast.LENGTH_SHORT).show();
-        }
-
-        @Override
-        public void onMarkerDrag(Marker marker) {
-            Toast.makeText(MapsActivity.this, "onMarkerDrag", Toast.LENGTH_SHORT).show();
-        }
-
-        @Override
-        public void onMarkerDragEnd(Marker marker) {
-            // getting the Co-ordinates
-            latitude = marker.getPosition().latitude;
-            longitude = marker.getPosition().longitude;
-
-            //move to current position
-            moveMap();
-        }
-
-        @Override
-        protected void onStart() {
-            googleApiClient.connect();
-            super.onStart();
-        }
-
-        @Override
-        protected void onStop() {
-            googleApiClient.disconnect();
-            super.onStop();
-        }
-
-
-        @Override
-        public boolean onMarkerClick(Marker marker) {
-            Toast.makeText(MapsActivity.this, "onMarkerClick", Toast.LENGTH_SHORT).show();
-            return true;
-        }
-
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        mMap = map;
+
+        mMap.setOnMyLocationButtonClickListener(this);
+        enableMyLocation();
+    }
+
+    /**
+     * Enables the My Location layer if the fine location permission has been granted.
+     */
+    private void enableMyLocation() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission to access the location is missing.
+            PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION, true);
+        } else if (mMap != null) {
+            // Access to the location has been granted to the app.
+            mMap.setMyLocationEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show();
+        // Return false so that we don't consume the event and the default behavior still occurs
+        // (the camera animates to the user's current position).
+        return false;
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+            return;
+        }
+
+        if (PermissionUtils.isPermissionGranted(permissions, grantResults,
+                android.Manifest.permission.ACCESS_FINE_LOCATION)) {
+            // Enable the my location layer if the permission has been granted.
+            enableMyLocation();
+        } else {
+            // Display the missing permission error dialog when the fragments resume.
+            mPermissionDenied = true;
+        }
+    }
+
+    @Override
+    protected void onResumeFragments() {
+        super.onResumeFragments();
+        if (mPermissionDenied) {
+            // Permission was not granted, display error dialog.
+            showMissingPermissionError();
+            mPermissionDenied = false;
+        }
+    }
+
+    /**
+     * Displays a dialog with error message explaining that the location permission is missing.
+     */
+    private void showMissingPermissionError() {
+        PermissionUtils.PermissionDeniedDialog
+                .newInstance(true).show(getSupportFragmentManager(), "dialog");
+    }
+
+}
