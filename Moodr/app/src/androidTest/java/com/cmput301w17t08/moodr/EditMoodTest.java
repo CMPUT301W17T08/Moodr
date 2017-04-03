3ab6ae2a21d91f9c364ca5aee739fd0aa22b73af
kirsten <kskwong@ualberta.ca>
2017-04-03 09:55:59 -0600

    edit mood test

--- Moodr/app/src/androidTest/java/com/cmput301w17t08/moodr/EditMoodTest.java
+++ Moodr/app/src/androidTest/java/com/cmput301w17t08/moodr/EditMoodTest.java
@@ -1,17 +1,17 @@
 package com.cmput301w17t08.moodr;
 
-/**
- * Created by kskwong on 3/13/17.
- */
 
 import android.app.Activity;
 import android.content.Intent;
 import android.test.ActivityInstrumentationTestCase2;
-import android.view.View;
-import android.widget.ListView;
+import android.util.Log;
 import android.widget.Spinner;
 
 import com.robotium.solo.Solo;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 /**
  *
@@ -23,11 +23,36 @@ public class EditMoodTest extends ActivityInstrumentationTestCase2<MyProfileActi
 
     public EditMoodTest() {
         super(com.cmput301w17t08.moodr.MyProfileActivity.class);
-        CurrentUserSingleton.getInstance().getUser().setName("bob");
-        CurrentUserSingleton.getInstance().getMyMoodList().add(new Mood("bob", Emotion.sad));
-        Intent intent = new Intent();
-        intent.putExtra("index", 0);
-        setActivityIntent(intent);
+        // log in
+        ElasticSearchUserController.GetUserTask getUser = new ElasticSearchUserController.GetUserTask();
+        getUser.execute("bob");
+
+        User user = new User();
+        try {
+            user = getUser.get().get(0);
+        } catch (Exception e) {
+            // nothing.
+        }
+
+        CurrentUserSingleton.getInstance().setSingleton(user);
+
+        // populate all current user's mood
+        ElasticSearchMoodController.GetMoodTask getMoodTask
+                = new ElasticSearchMoodController.GetMoodTask();
+        ArrayList<Mood> moods = new ArrayList<>();
+        getMoodTask.execute("bob");
+        try {
+            moods.addAll(getMoodTask.get());
+            Collections.sort(moods, new Comparator<Mood>() {
+                @Override
+                public int compare(Mood mood, Mood t1) {
+                    return t1.getDate().compareTo(mood.getDate());
+                }
+            });
+        } catch (Exception e) {
+            Log.d("Error", "Error getting moods from elastic search.");
+        }
+        CurrentUserSingleton.getInstance().getMyMoodList().setListOfMoods(moods);
     }
 
     public void setUp() throws Exception {
@@ -39,8 +64,8 @@ public class EditMoodTest extends ActivityInstrumentationTestCase2<MyProfileActi
     }
 
     // test loading
-    public  void testLoad(){
-       solo.clickOnText("Sad");
+    public void testLoad() {
+        solo.clickInList(1, 1);
         solo.assertCurrentActivity("Wrong activity", ViewMyMoodActivity.class);
 
 //        solo.clickOnActionBarItem(R.id.edit_mood);  does not click on this for some reason...
@@ -49,26 +74,31 @@ public class EditMoodTest extends ActivityInstrumentationTestCase2<MyProfileActi
         solo.assertCurrentActivity("Wrong activity", EditMoodActivity.class);
 
     }
+
     // test editing emotion
-    public void testEditEmotion(){
-        solo.clickOnText("Sad");
+    public void testEditEmotion() {
+        solo.clickInList(1, 1);
         solo.assertCurrentActivity("Wrong activity", ViewMyMoodActivity.class);
-        solo.clickOnView(solo.getView(R.id.action_edit_complete));
-        solo.pressSpinnerItem(0,1);
+
+        solo.clickOnView(solo.getView(R.id.edit_mood
+        ));
+        solo.pressSpinnerItem(0, 1);
 
         Spinner spinner = (Spinner) solo.getView(R.id.sp_emotion);
 
-        String string  = spinner.getSelectedItem().toString();
+        String string = spinner.getSelectedItem().toString();
 
-        solo.clickOnScreen(850 ,174);
+        solo.clickOnView(solo.getView(R.id.action_edit_complete));
 
+        solo.waitForActivity(ViewMyMoodActivity.class);
         solo.waitForText(string);
     }
 
 
     @Override
-    public void tearDown() throws Exception{
+    public void tearDown() throws Exception {
         solo.finishOpenedActivities();
     }
 
 }
+
